768b33cb9614e440cfa39b050c0114944953b7f4
FinnianLR <finnniansproject@gmail.com>
2023-01-30 23:47:09 +1300

    Edited ReadMe

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+
+      <div id="navigation-bar">
+        <nav>
+          <ul>
+            <li><a href="#" id="logo">Home</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Skills</a></li>
+          </ul>
+        </nav>
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className=''>Hello! My name is Finnian and this is my portfolio.</h1>
       </header>
+      <div className='arrowDiv'>
+        <div className="bottom-arrow"></div>
+      </div>
+      <h3>dsa</h3>
+      <ul>
+        <li>Projects</li>
+        <li>Skills</li>
+      </ul>
+
     </div>
   );
 }
